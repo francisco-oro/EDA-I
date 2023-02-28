569c946cff6ebd70d9d71f4c25fb4eb250fbce10
FRANCISCO ABIMAEL ORO ESTRADA <francisco.oro@comunidad.unam.mx>
2023-02-28 13:09:55 -0600

    Act 8: Comments

--- practica01/actividad8.c
+++ practica01/actividad8.c
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
     {
         for (j = 0; j < 3; j++)
         {
-            A[2][i][j] = 0;
+            A[2][i][j] = 0;   //Llenar la matriz producto con 0's
         }
         
     }
@@ -28,7 +28,7 @@ int main(int argc, char const *argv[])
             for (k = 0; k < 3; k++)
             {
                 A[i][j][k] = (rand()%10);
-                printf("%d ", A[i][j][k]);
+                printf("%d ", A[i][j][k]);  //Lenar matrices 0 y 1 con números aleatorios
             }
             printf("\n");
         }
@@ -41,7 +41,7 @@ int main(int argc, char const *argv[])
         {   
             for (k = 0; k < 3; k++)
             {
-                A[2][i][j] += (A[0][i][k] * A[1][k][j]);
+                A[2][i][j] += (A[0][i][k] * A[1][k][j]);  //Multiplicación de matrices
                 // printf("i: %d j: %d k: %d\n", i, j, k);
                 // printf("%d * %d \n",A[0][i][k], A[1][k][i]);
             }
@@ -52,7 +52,7 @@ int main(int argc, char const *argv[])
     {
         for (j = 0; j < 3; j++)
         {
-            printf("%d ", A[2][i][j]);
+            printf("%d ", A[2][i][j]); //Imprime el producto de ambas matrices
         }
         printf("\n");
     }
