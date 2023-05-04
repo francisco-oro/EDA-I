9015fec9ea420d55b15c477b34056e653f906bf8
Francisco <francisco.oro@comunidad.unam.mx>
2023-05-04 09:58:01 -0600

    practica09

--- practica09/main.c
+++ practica09/main.c
@@ -1,8 +1,21 @@
+/******************************************************************************
+Francisco Abimael Oro Estrada
+N.C. 423115639
+Practica 9 - Actividad 1
+*******************************************************************************/
+
 #include <stdio.h>
 #include "SQcar.h"
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    tpila s1;
+    tq qsal;
+    int i, nc=0;
+    char car, caux;
+    char *exp;
+    
     return 0;
 }
