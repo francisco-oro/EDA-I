10f88713503513a35f21a8b921bc57243f6dbcc0
Francisco <francisco.oro@comunidad.unam.mx>
2023-05-10 22:42:48 -0600

    lab07-test3

--- practica07/actividad3.c
+++ practica07/actividad3.c
@@ -7,124 +7,136 @@ Practica 7 - Actividad 3
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{ 
-    float dato; 
-    struct node *sig;
+struct node
+{
+    float value;
+    struct node *sig; 
 };
 
 typedef struct node nodo;
 
-struct tipoCol{
-    nodo *ini;
-    nodo *fin; 
+struct tipoCola
+{
+    nodo *ini, *fin;
     int tam; 
-}; 
+};
+typedef struct tipoCola queue; 
 
-typedef struct tipaCala tq; 
-
-int vacia(tq *q){ 
-    if(q->ini==NULL ii q->fin==NULL && q->tar==0) 
-    return 0; 
-    else return 1; 
+void startQueue(queue *queue){
+    // Los nodos a inicio y fin son nulos, mientras que el tamaño de la cola deberá ser de 0 elementos
+    queue->fin = NULL;
+    queue->ini = NULL;
+    queue->tam = 0;
 }
 
-int encolar(iq *q, float dato){ 
-    nodo *nuevoNodo; 
-    nuevoNodo=(nodo *)malloc(sizeof(nodo)); 
-    if(nuevoNodo==NULL) return 1; 
-    nuevoNodo->dato=dato; 
-    nuevoNodo->sig=NULL; 
-    if(!vacia(q)){
-        q->fin=nuevoNodo; 
-        q->ini=nuevoNodo; 
-    } else{ 
-        q->fin->sig=nuevoNodo; 
-        q->fin+nuevoNodo; 
+int vacia(queue *tq){
+    // Regresa verdadero si la cola está vacía
+    if (tq->ini == NULL && tq->fin == NULL && tq->tam == 0)
+    {
+        return 1; 
+    } 
+    // False if queue is empty
+    else
+    {
+        return 0; 
     }
-    q->tam++; 
-    return 0;
 }
 
-
-float desencolar(tq *q){ 
-    nodo *tmp; 
-    float dato; 
-    if(!vacia(q)) return -1.0; 
-    tmp=q->ini; 
-    if(q->ini==q->fin) creaCola(q); 
-    else q->ini=q->ini->nig; 
-    q->tan--; 
-    dato tmp->dato; 
-    free(tmp); 
-    dato; 
-}
-
-int main(){
-    tpila word;
-    int i, j;
-    char test1[5]="kayak", test2[5];
-    //Crear dos pilas
-    creaPila(&word);
-
-    //Agregar las palabras a cada stack
-    for(i=0;i<5;i++){
-        push(&word, test1[i]);
-    }
-
-    for(i=0;i<5;i++){
-        test2[i]=pop(&word);
-    }
-
-    for(i=0;i<5;i++){
-        if(test2[i]==test1[i])j++;
-    }
-    if(j==5) printf("La palabra es palindrome");
-    else printf("La palabra no es palindrome");
-    return 0;
-}
-
-void creaPila(tpila *stack){ 
-    stack->tope=NULL; 
-    stack->tam=0; 
-}
-
-int vacia(tpila *stack){
-    if(stack->tope==NULL && stack->tam==0) return 0; 
-    else return 1; 
-}
-
-int push (tpila *stack, char dato){ 
-    nodo *nuevoNodo; 
-    nuevoNodo = (nodo *)malloc(sizeof(nodo));
-    if(nuevoNodo==NULL){ 
+int enqueue(queue *queue, float val){
+    nodo *newNode;
+    newNode = (nodo *)malloc(sizeof(nodo));
+    // Stop the function if memory allocation fails
+    if (newNode == NULL)
+    {
         return 1; 
     }
-    nuevoNodo->dato=dato; 
-    nuevoNodo->sig=stack->tope; 
-    stack->tope=nuevoNodo; 
-    stack->tam++; 
+    // Assign the value to the new node and set its next element as NULL
+    newNode->value = val;
+    newNode->sig = NULL;
+    // If the queue is empty, assign the new node to its front and rear
+    if (vacia(queue))
+    {
+        queue->fin = newNode;
+        queue->ini = newNode;
+    }
+    // If the queue is not empty: (1) assign the new node as the next of the last node in the list (2) Update the new Node as the last item
+    else
+    {
+        queue->fin->sig = newNode;
+        queue->fin = newNode;
+    }
+    // Increase the size of the queue
+    queue->tam++;
     return 0; 
 }
 
-float pop(tpila *stack){
-    nodo *tmp; 
-    char dato; 
-    if(!vacia(stack)) return -1.0; 
-    tmp=stack->tope; 
-    stack->tope=stack->tope->sig; 
-    stack->tam--; 
-    dato=tmp->dato; 
-    free(tmp); 
-    return dato; 
+float dequeue(queue *queue){
+    nodo *tmp;
+    float returnValue;
+    // Stop the function if the queue is empty
+    if (vacia(queue))
+    {
+        printf("This queue is empty\n");
+        return -1;
+    }
+    // Retrieve the first item in the queue and assign it to the temporary node
+    tmp = queue->ini;
+    // Restart the queue if there's only one item left in it before removing it
+    if (queue->fin == queue ->fin && queue->tam == 1) 
+    {
+        startQueue(queue);
+    }
+    // If there's more than one element in the queue (1) Update the second item as the new first item in the list (2) Decrease its size by 1 unit
+    else
+    {
+        queue->ini = queue->ini->sig;
+        queue->tam--;
+    }
+    // Return the removed value and release the memory to the system
+    returnValue = tmp->value;
+    free(tmp);
+    return returnValue;
 }
 
-void mostrar( tpila *stack){
-    nodo *tmp; 
-    tmp=stack->tope; 
+void display(queue *queue){
+    nodo *tmp;
+    tmp = queue->ini;
+    // Loop through the items in the queue
+    for (size_t i = 0; i < queue->tam; i++)
+    {
+        printf("%f ", tmp->value);
+        // Update the item as the next one
+        tmp = tmp->sig;
+    }   
     printf("\n");
-    while(tmp!=NULL){
-        printf("%c",tmp->dato);
-        tmp=tmp->sig;
+}
+
+int main(int argc, char const *argv[])
+{
+    queue queue1;
+    
+    startQueue(&queue1);
+    printf("enqueue 5 elements : \n");
+    for (size_t i = 0; i < 5; i++)
+    {
+        enqueue(&queue1, i);
     }
-    printf("\n");
+    display(&queue1);
+    printf("----------------------------------"); 
+
+    printf("\nDequeue 2 elements : \n");
+    for (size_t i = 0; i < 2; i++)
+    {
+        printf("Removed %.2f from the queue \n", dequeue(&queue1));
+    }
+    printf("----------------------------------"); 
+
+    printf("\nDequeue 4 elements : \n");
+    for (size_t i = 0; i < 4; i++)
+    {
+        printf("Removed %.2f from the queue\n", dequeue(&queue1)); 
+    }
+    printf("----------------------------------"); 
+
+    return 0;
 }
