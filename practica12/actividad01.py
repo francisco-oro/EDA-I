b6b745cebc9c1370ead13ccbda75cdea8cdafe77
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2023-05-25 16:49:33 +0000

    practica11

--- practica12/actividad01.py
+++ practica12/actividad01.py
@@ -1,17 +1,8 @@
-"""
-Francisco Abimael Oro Estrada
-N.C. 423115639
-Practica 11 - Actividad 5 
-"""
 def fact(n):
-    if n == 0 or n==1:
+    if n == 0 or n == 1:
         return 1
-    else: 
-        return n * fact(n - 1)
+    return n *fact(n - 1)
 
-if __name__ == '__main__':
-    num =  int(input("Input a number : "))
-    print(f"The factorial of {num} is : {fact(num)}")
 
 if __name__ == '__main__':
     num =  int(input("Input a number : "))
