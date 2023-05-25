81a517bb73ed8249d090e37d4d8617bd24277281
Francisco <francisco.oro@comunidad.unam.mx>
2023-05-24 22:44:10 -0600

    practica10&act04

--- practica11/actividad04.py
+++ practica11/actividad04.py
@@ -2,25 +2,21 @@
 # N.C. 423115639
 # Practica 10 - Actividad 4
 
-import random as rm
+import math
+import random as rd
 
-def linsearch(A, n, x): 
-    for i in range(n):
-        print(i)
-        if x == A[i]:
-            return i
-    return -1
+def closestPair(P):
+    minimum_distance = 1000
+    for i in range(len(P)):
+        for j in range(i+1, len(P)):
+            distance = math.sqrt((P[i][0] - P[j][0]) ** 2 + (P[i][1] - P[j][1]) ** 2)
+            if distance < minimum_distance:
+                minimum_distance = distance
+    return minimum_distance
 
-def main():
-    # Generate a random set of 10 numbers between 1 and 30
-    A = []
-    for i in range(10):
-        A.append(rm.randint(1,30))
+# Using a list of tuples to test the closest pair 
 
-    # Generate a random number to look for
-    wanted = rm.randint(1,30)
-    
-    print(f"Generated the following sequence of numbers : {A}")
-    print(f"The number {wanted} is in the set A  at index: {linsearch(A, len(A), wanted)}")
-
-main()
+random_points = [(rd.randint(0,10), rd. randint(0,10)) for i in range(10)]
+print("Generated the following set of ordered pairs : ")
+print(random_points)
+print(f"The minimum distance is : {closestPair(random_points)}")
